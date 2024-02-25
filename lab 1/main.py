8ec5cd1ad485521c483e757e2bfb50673840deb4
Renat03 <grsrenat@gmail.com>
2024-02-25 22:28:45 +0200

    Cosmetic changes

--- lab 1/main.py
+++ lab 1/main.py
@@ -8,32 +8,39 @@ class Grammar:
         self.rules = p
         self.start_symbol = s
 
-    def generate_strings(self):
+    def generate_strings(self, count):
         words = []
-        while len(words) != 5:
-            str = "S"
-            while str.lower() != str:
-                str = str.replace(str[len(str) - 1],
-                                  self.rules[str[len(str) - 1]][random.randint(0, len(self.rules[str[len(str) - 1]]) - 1)])
-            if str not in words:
-                words.append(str)
+        while len(words) != count:
+            word = "S"
+            while word.lower() != word:
+                random_transition = random.randint(0, len(self.rules[word[-1]]) - 1)
+                word = word.replace(word[-1], self.rules[word[-1]][random_transition])
+            if word not in words:
+                words.append(word)
         return words
 
-    def to_automaton(self):
+    def to_finite_automaton(self):
         states = self.non_terminals + ['X']
         alphabet = self.terminals
-        transition_function = {}
-        transition_function['X'] = {}
+        transition_function = {'X': {}}
         start = self.start_symbol
         accept = ['X']
 
         for non_terminal in self.non_terminals:
             transition_function[non_terminal] = {}
-            for production in self.rules.get(non_terminal, []):
+            for production in self.rules[non_terminal]:
                 if len(production) == 1:
                     transition_function[non_terminal][production] = 'X'
                 elif len(production) == 2:
                     transition_function[non_terminal][production[0]] = production[1]
+
+
+        # print(f"states: {states}")
+        # print(f"alphabet: {alphabet}")
+        # print(f"transition function: {transition_function}")
+        # print(f"start: {start}")
+        # print(f"accept: {accept}")
+
 
         return FiniteAutomaton(states, alphabet, transition_function, start, accept)
 
@@ -46,36 +53,35 @@ class FiniteAutomaton:
         self.start = start
         self.accept = accept
 
-    def check(self, word):
+    def string_belong_to_language(self, word):
         current_state = self.start
         for char in word:
-            if char in self.transition_function.get(current_state, {}):
+            if char in self.transition_function[current_state]:
                 current_state = self.transition_function[current_state][char]
             else:
                 return False
         return current_state in self.accept
 
 
-if __name__ == "__main__":
-    s = 'S'
-    vn = ['S', 'L', 'B']
-    vt = ['a', 'b', 'c']
-    p = {
-        'S': ['aB'],
-        'B': ['bB', 'cL'],
-        'L': ['cL', 'aS', 'b']
-    }
+s = 'S'
+vn = ['S', 'L', 'B']
+vt = ['a', 'b', 'c']
+p = {
+    'S': ['aB'],
+    'B': ['bB', 'cL'],
+    'L': ['cL', 'aS', 'b']
+}
 
-    grammar = Grammar(vn, vt, p, s)
-    generated_words = grammar.generate_strings()
-    print("Generated words:")
-    print(generated_words)
+grammar = Grammar(vn, vt, p, s)
+generated_words = grammar.generate_strings(5)
+print("Generated words:")
+print(generated_words)
 
-    print("\nCheck if the words are accepted:")
-    for i in generated_words:
-        print(f"{i} - {grammar.to_automaton().check(i)}")
+print("\n\nCheck if the words are accepted:")
+for i in generated_words:
+    print(f"{i} - {grammar.to_finite_automaton().string_belong_to_language(i)}")
 
-    print("\nChecking random words:")
-    random_words = ['check', 'word', 'cab']
-    for i in random_words:
-        print(f"{i} - {grammar.to_automaton().check(i)}")
+print("\n\nChecking random words:")
+random_words = ['check', 'word', 'cab', 'abc', 'acb']
+for i in random_words:
+    print(f"{i} - {grammar.to_finite_automaton().string_belong_to_language(i)}")
