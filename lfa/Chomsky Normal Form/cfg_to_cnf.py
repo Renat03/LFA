8c38080fea66ecbedbcd8502aec2f6bcdea6b9a8
Renat03 <grsrenat@gmail.com>
2024-04-29 17:05:17 +0300

    Add implementation for fifth laboratory work

--- lfa/Chomsky Normal Form/cfg_to_cnf.py
+++ lfa/Chomsky Normal Form/cfg_to_cnf.py
@@ -1,178 +1,153 @@
-class BreakFromLoops(Exception):
-    pass
+import unittest
 
 
-class Grammar():
-    def __init__(self, vn, vt, p, s):
-        self.Vn = vn
-        self.Vt = vt
-        self.P = p
-        self.S = s
+class Grammar:
+    def __init__(self, non_terminals, terminals, productions, start_symbol):
+        self.non_terminals = non_terminals
+        self.terminals = terminals
+        self.productions = productions
+        self.start_symbol = start_symbol
 
-    def print_grammar(self):
-        print('Vn:', self.Vn)
-        print('Vt:', self.Vt)
-        print('P:')
-        for key, value in self.P.items():
-            print(f'{key} -> {value}')
-        print('S: ', self.S, '\n')
+    def remove_start_symbol_from_rhs(self):
+        if any(self.start_symbol in production for values in self.productions.values() for production in values):
+            new_start_symbol = 'X'
+            self.productions[new_start_symbol] = [self.start_symbol]
+            self.start_symbol = new_start_symbol
+            self.non_terminals.append(new_start_symbol)
 
-    def cfg_to_cnf(self):
-        self.start_symbol_rhs_removal()
-        self.remove_null_productions()
-        self.remove_unit_productions()
-        self.remove_inaccessible_symbols()
-        self.replace_terminals_with_nonterminals()
-        self.reduce_production_length()
+    def create_new_productions(self, production, symbol):
+        return [production[:i] + production[i+1:] for i in range(len(production)) if production[i] == symbol]
 
-    def start_symbol_rhs_removal(self):
-        try:
-            for value in self.P.values():
-                for production in value:
-                    for character in production:
-                        if character == self.S:
-                            raise BreakFromLoops
-        except:
-            new_P = {'X': [self.S]}
-            new_P.update(self.P)
-            self.P = new_P
-            self.S = 'X'
-            self.Vn.append(self.S)
+    def eliminate_null_productions(self):
+        null_producing_symbols = {key for key, values in self.productions.items() if 'ε' in values}
+        for symbol in null_producing_symbols:
+            self.productions[symbol].remove('ε')
+            for key, values in list(self.productions.items()):
+                new_productions = [new_prod for production in values if symbol in production for new_prod in self.create_new_productions(production, symbol)]
+                self.productions[key].extend(new_prod for new_prod in new_productions if new_prod not in values)
 
-    def create_new_productions(self, production, character):
-        results = []
-        for i in range(len(production)):
-            if production[i] == character:
-                new_production = production[:i] + production[i+1:]
-                results.append(new_production)
-        return results
-
-    def remove_null_productions(self):
-        count_null = 0
-        null_prods = []
-        for key, value in self.P.items():
-            for production in value:
-                if production == 'ε':
-                    count_null += 1
-                    null_prods.append(key)
-                    value.remove(production)
-        for i in range(count_null):
-            new_P = self.P
-            for key, value in self.P.items():
-                for production in value:
-                    for character in production:
-                        if character == null_prods[i]:
-                            new_productions = self.create_new_productions(production, null_prods[i])
-                            for j in new_productions:
-                                if j not in new_P[key]:
-                                    new_P[key].append(j)
-                            break
-            self.P = new_P
-
-    def remove_unit_productions(self):
-        for key, value in self.P.items():
-            for production in value:
-                if key == production:
-                    self.P[key].remove(production)
+    def eliminate_unit_productions(self):
         changes = True
         while changes:
             changes = False
-            for key, value in self.P.items():
-                for production in value:
-                    if production in self.Vn:
-                        changes = True
-                        self.P[key].remove(production)
-                        for prod in self.P[production]:
-                            if prod not in self.P[key]:
-                                self.P[key].append(prod)
+            for key, values in list(self.productions.items()):
+                unit_productions = [prod for prod in values if prod in self.non_terminals and prod != key]
+                for production in unit_productions:
+                    changes = True
+                    values.remove(production)
+                    values.extend(new_prod for new_prod in self.productions[production] if new_prod not in values)
 
-    def remove_inaccessible_symbols(self):
-        accessible = set([self.S])
-        queue = [self.S]
-
+    def eliminate_inaccessible_symbols(self):
+        accessible = {self.start_symbol}
+        queue = [self.start_symbol]
         while queue:
             current = queue.pop(0)
-            for production in self.P.get(current, []):
+            for production in self.productions.get(current, []):
                 for symbol in production:
-                    if symbol in self.Vn and symbol not in accessible:
+                    if symbol in self.non_terminals and symbol not in accessible:
                         accessible.add(symbol)
                         queue.append(symbol)
 
-        self.Vn = [nt for nt in self.Vn if nt in accessible]
-        for nt in list(self.P.keys()):
-            if nt not in accessible:
-                del self.P[nt]
+        self.non_terminals = [nt for nt in self.non_terminals if nt in accessible]
+        self.productions = {key: values for key, values in self.productions.items() if key in accessible}
 
-
-    def replace_terminals_with_nonterminals(self):
-        def new_nonterminal(existing):
-            for char in (chr(i) for i in range(65, 91)):
-                if char not in existing:
-                    return char
-            raise ValueError("Ran out of single-letter nonterminal symbols!")
-
+    def substitute_terminals_in_non_single_productions(self):
         terminal_to_nonterminal = {}
-        new_P = {}
-        for key, productions in self.P.items():
-            new_productions = []
-            for prod in productions:
-                if len(prod) > 1:
-                    new_prod = ''
-                    for char in prod:
-                        if char in self.Vt:
+        for key, productions in list(self.productions.items()):
+            for production in productions:
+                if len(production) > 1:
+                    new_production = ''
+                    for char in production:
+                        if char in self.terminals:
                             if char not in terminal_to_nonterminal:
-                                new_nt = new_nonterminal(self.Vn)
-                                self.Vn.append(new_nt)
-                                terminal_to_nonterminal[char] = new_nt
-                                new_P[new_nt] = [char]
-                            new_prod += terminal_to_nonterminal[char]
+                                new_nonterminal = next(chr(i) for i in range(65, 91) if chr(i) not in self.non_terminals)
+                                self.non_terminals.append(new_nonterminal)
+                                terminal_to_nonterminal[char] = new_nonterminal
+                                self.productions[new_nonterminal] = [char]
+                            new_production += terminal_to_nonterminal[char]
                         else:
-                            new_prod += char
-                    new_productions.append(new_prod)
-                else:
-                    new_productions.append(prod)
-            new_P[key] = new_productions
-        self.P.update(new_P)
+                            new_production += char
+                    productions[productions.index(production)] = new_production
 
-    def reduce_production_length(self):
-        def new_nonterminal(existing):
-            for char in (chr(i) for i in range(65, 91)):
-                if char not in existing:
-                    return char
-            raise ValueError("Ran out of single-letter nonterminal symbols!")
-
-        existing_binaries = {}
-        new_productions_dict = {}
-        for key, productions in list(self.P.items()):
-            new_productions = []
+    def shorten_productions(self):
+        binary_nonterminal_map = {}
+        for key, productions in list(self.productions.items()):
             for production in productions:
                 if len(production) > 2:
-                    while len(production) > 2:
-                        last_two = production[-2:]
-                        if last_two not in existing_binaries:
-                            new_nt = new_nonterminal(set(self.Vn) | set(new_productions_dict.keys()))
-                            self.Vn.append(new_nt)
-                            new_productions_dict[new_nt] = [last_two]
-                            existing_binaries[last_two] = new_nt
-                        production = production[:-2] + existing_binaries[last_two]
-                    new_productions.append(production)
-                else:
-                    new_productions.append(production)
-            self.P[key] = new_productions
-        self.P.update(new_productions_dict)
+                    new_production = production
+                    while len(new_production) > 2:
+                        last_two = new_production[-2:]
+                        if last_two not in binary_nonterminal_map:
+                            new_nonterminal = next(chr(i) for i in range(65, 91) if chr(i) not in self.non_terminals)
+                            self.non_terminals.append(new_nonterminal)
+                            binary_nonterminal_map[last_two] = new_nonterminal
+                            self.productions[new_nonterminal] = [last_two]
+                        new_production = new_production[:-2] + binary_nonterminal_map[last_two]
+                    productions[productions.index(production)] = new_production
 
-# Define the grammar
-vn = ['S', 'A', 'B', 'D']
-vt = ['a', 'b', 'd']
-p = {
+    def convert_to_cnf(self):
+        self.remove_start_symbol_from_rhs()
+        self.eliminate_null_productions()
+        self.eliminate_unit_productions()
+        self.eliminate_inaccessible_symbols()
+        self.substitute_terminals_in_non_single_productions()
+        self.shorten_productions()
+
+
+class TestGrammarMethods(unittest.TestCase):
+    def setUp(self):
+        self.grammar = Grammar(['S', 'A', 'B', 'D'], 
+            ['a', 'b', 'd'], 
+        {
+            'S': ['dB', 'AB'],
+            'A': ['d', 'dS', 'ε', 'aAaAb'],
+            'B': ['a', 'aS', 'A'],
+            'D': ['Aba']
+        }, 
+        'S')
+
+    def test_remove_start_symbol_from_rhs(self):
+        self.grammar.remove_start_symbol_from_rhs()
+        self.assertNotIn(self.grammar.start_symbol, [prod for values in self.grammar.productions.values() for prod in values])
+        self.assertIn('X', self.grammar.non_terminals)
+
+    def test_eliminate_null_productions(self):
+        self.grammar.eliminate_null_productions()
+        for values in self.grammar.productions.values():
+            self.assertNotIn('ε', values)
+
+    def test_eliminate_unit_productions(self):
+        self.grammar.eliminate_unit_productions()
+        for values in self.grammar.productions.values():
+            for prod in values:
+                self.assertFalse(len(prod) == 1 and prod.isupper())
+
+    def test_shorten_production(self):
+        self.grammar.substitute_terminals_in_non_single_productions()
+        self.grammar.shorten_productions()
+        for values in self.grammar.productions.values():
+            for prod in values:
+                self.assertTrue(len(prod) <= 2)
+
+#if __name__ == '__main__':
+#    unittest.main()
+
+non_terminals = ['S', 'A', 'B', 'D']
+terminals = ['a', 'b', 'd']
+productions = {
     'S': ['dB', 'AB'],
-    'A': ['d', 'dS','ε', 'aAaAb'],
+    'A': ['d', 'dS', 'ε', 'aAaAb'],
     'B': ['a', 'aS', 'A'],
     'D': ['Aba']
 }
-s = 'S'
+start_symbol = 'S'
 
-# Create the Grammar object and print the CNF
-grammar = Grammar(vn, vt, p, s)
-grammar.cfg_to_cnf()
-grammar.print_grammar()
+grammar = Grammar(non_terminals, terminals, productions, start_symbol)
+grammar.convert_to_cnf()
+
+print('Non-terminals:', grammar.non_terminals)
+print('Terminals:', grammar.terminals)
+print('Productions:')
+for key, value in grammar.productions.items():
+    print(f'{key} -> {value}')
+print('Start Symbol: ', grammar.start_symbol, '\n')
